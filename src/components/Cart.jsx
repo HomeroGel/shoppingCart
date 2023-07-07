e447a3e7ed3b2685b3b24da13537a812e02c3482
Homero Gelves <homerogelves@gmail.com>
2023-07-07 18:20:29 -0300

    cart with context finish

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,41 +1,57 @@
 import { useId } from "react";
 import { CartIcon, ClearCartIcon } from "./Icons";
 import './Cart.css'
+import { useCart } from "../hooks/useCart";
+
+
+// eslint-disable-next-line react/prop-types
+function CartItem ({thumbnail, price, title, quantity, addToCart}) {
+    return(
+        <li>
+        <img
+            src={thumbnail}
+            alt={title}
+        />
+        <div>
+            <strong>{title}</strong> - ${price}
+        </div>
+
+        <footer>
+            <small>
+                Qty: {quantity}
+            </small>
+            <button onClick={addToCart}>+</button>
+        </footer>
+    </li>
+    )
+}
 
 export const Cart = () => {
     const cartCheckboxId = useId()
-  
+    const { clearCart, cart, addToCart } = useCart()
+
     return (
-    <>
-        <label className="cart-button" htmlFor={cartCheckboxId}>
-            <CartIcon />
-        </label>
-        <input id={cartCheckboxId} type="checkbox" hidden />
+        <>
+            <label className="cart-button" htmlFor={cartCheckboxId}>
+                <CartIcon />
+            </label>
+            <input id={cartCheckboxId} type="checkbox" hidden />
 
-        <aside className="cart">
-            <ul>
-                <li>
-                    <img 
-                        src="https://i.dummyjson.com/data/products/2/thumbnail.jpg" 
-                        alt="Iphone" 
-                    />
-                    <div>
-                        <strong>iPhone</strong> - $1499
-                    </div>
+            <aside className="cart">
+                <ul>
+                    {cart.map(product => (
+                        <CartItem 
+                            key={product.id}
+                            addToCart={()=>addToCart(product)}
+                            {...product}
+                        />
+                    ))}
+                </ul>
 
-                    <footer>
-                        <small>
-                            Qty: 1
-                        </small>
-                        <button>+</button>
-                    </footer>
-                </li>
-            </ul>
-
-            <button>
-                <ClearCartIcon />
-            </button>
-        </aside>
-    </>
-  )
+                <button onClick={clearCart}>
+                    <ClearCartIcon />
+                </button>
+            </aside>
+        </>
+    )
 }
